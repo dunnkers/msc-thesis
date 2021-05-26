fccc53b9b34a971cc01249d830928fdda608171f
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-05-26 10:02:14 +0200

    🔨

--- src/sacct_to_csv.sh
+++ src/sacct_to_csv.sh
@@ -2,7 +2,6 @@ helpformat=$(sacct --helpformat)
 params=$(echo $helpformat | sed 's/ /,/g')
 sacct --format="$params" \
     --starttime 2021-05-20 \
-    # --units=M \
     --jobs=$1 \
     -u $USER \
     --parsable2 \
