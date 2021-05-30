34a532553356b82f2772cc918561aa2017805df7
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-05-30 18:21:37 +0200

    🔨

--- jobs/rq_worker.sh
+++ jobs/rq_worker.sh
@@ -20,8 +20,5 @@ module load Python/3.8.6-GCCcore-10.2.0
 venv_dir=$TMPDIR/venv_$SLURM_JOB_ID
 python -m venv $venv_dir
 source $venv_dir/bin/activate
-pip install rq
-pip install -e git+https://github.com/dunnkers/hydra.git@master#egg=hydra_rq_launcher&subdirectory=plugins/hydra_rq_launcher
 pip install git+https://github.com/dunnkers/fseval.git@master
-pip install hydra-core
 rq worker -u $REDIS_URL
