a2a27aeebbd925e3bd1893ebfa0a675bcefad262
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-06-01 10:17:37 +0200

    🔨 put braces around env variable

--- jobs/_prepare_env.sh
+++ jobs/_prepare_env.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-echo "preparing fseval environment in directory:"
+echo "preparing \e[33mfseval environment\e[0m in directory:"
 pwd
 
 module load Python/3.8.6-GCCcore-10.2.0
 venv_dir=$TMPDIR/venv_${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}
 
-echo "creating clean virtual environment (in $venv_dir)..."
+echo -e "creating clean \e[33mvirtual environment\e[0m (in $venv_dir)..."
 rm -rf $venv_dir
 python -m venv $venv_dir
 source $venv_dir/bin/activate
-echo "virtual environment created  ✓"
+echo -e "virtual environment created  \e[32m✓\e[0m"
 
-echo "upgrading pip..."
+echo -e "upgrading \e[33mpip\e[0m..."
 python -m pip install --upgrade pip --quiet
-echo "pip upgraded ✓"
+echo -e "pip upgraded \e[32m✓\e[0m"
 
-echo "installing hydra rq launcher..."
+echo -e "installing \e[33mhydra rq launcher\e[0m..."
 pip install -e git+https://github.com/dunnkers/hydra.git@plugins/rq-launcher/fail-hard#"egg=hydra_rq_launcher&subdirectory=plugins/hydra_rq_launcher"  --quiet
-echo "hydra rq launcher installed ✓"
+echo -e "hydra rq launcher installed \e[32m✓\e[0m"
 
-echo "installing fseval..."
+echo -e "installing \e[33mfseval\e[0m..."
 pip install git+https://github.com/dunnkers/fseval.git@master --quiet
-echo "fseval installed ✓"
+echo -e "fseval installed \e[32m✓\e[0m"
--- jobs/_spawn_upload_logs_job.sh
+++ jobs/_spawn_upload_logs_job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ -n "${SLURM_ARRAY_TASK_ID+set}" ]; then
-    JOB_ID=$SLURM_JOB_ID_$SLURM_ARRAY_TASK_ID
+    JOB_ID=${SLURM_JOB_ID}_${SLURM_ARRAY_TASK_ID}
+    echo -e "→ current job is \e[33mpart of a job array\e[0m."
 else
-    JOB_ID=$SLURM_JOB_ID
+    JOB_ID=${SLURM_JOB_ID}
+    echo -e "→ current job is \e[33mstandalone\e[0m."
 fi
 
 echo "Scheduling 'logs upload' job as a dependency on job: $JOB_ID"
