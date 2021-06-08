ab8022fe7306af40f2c78f097088d51b93e05ef3
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-06-08 15:41:28 +0200

    fix

--- jobs/_get_run_path.sh
+++ jobs/_get_run_path.sh
@@ -1,4 +1,12 @@
 cd /scratch/$PEREGRINE_USERNAME/fseval/multirun
 run_dir=$(find ./ -maxdepth 5 -name *$1 | tail -n 1)
 run_dir_abs=$(realpath $run_dir)
-echo $run_dir_abs/files
+run_dir_files=$run_dir_abs/files
+has_pickle=$(find $run_dir_files -name *.pickle -print | head -n 1 | wc -l)
+
+if [ -d "$run_dir_files" ] && [$has_pickle > 0]; then
+    echo $run_dir_files
+else
+    echo ""
+fi
+
