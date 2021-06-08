fdb19f8646c48c626f5ab706afae66873f3448e3
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-06-08 15:43:58 +0200

    ✨

--- jobs/_get_run_path.sh
+++ jobs/_get_run_path.sh
@@ -1,10 +1,9 @@
-cd /scratch/$PEREGRINE_USERNAME/fseval/multirun
-run_dir=$(find ./ -maxdepth 5 -name *$1 | tail -n 1)
+run_dir=$(find /scratch/$PEREGRINE_USERNAME/fseval/multirun -maxdepth 5 -name * | tail -n 1)
 run_dir_abs=$(realpath $run_dir)
 run_dir_files=$run_dir_abs/files
 has_pickle=$(find $run_dir_files -name *.pickle -print | head -n 1 | wc -l)
 
-if [ -d "$run_dir_files" ] && [$has_pickle > 0]; then
+if [ -d "$run_dir_files" ] && ["$has_pickle" -gt 0]; then
     echo $run_dir_files
 else
     echo ""
