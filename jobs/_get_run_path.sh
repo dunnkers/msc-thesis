0b1dbb8ad40250cd92d1608f451a15cbf5518b89
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-06-21 22:37:32 +0200

    Update script

--- jobs/_get_run_path.sh
+++ jobs/_get_run_path.sh
@@ -5,7 +5,7 @@ search_dir=/scratch/$PEREGRINE_USERNAME/fseval/multirun
 run_id="$1"
 
 # CSV header
-echo "hydra_dir,wandb_dir,hydra_pickles,wandb_pickles"
+echo "run_dir,storage_dir,n_pickles"
 
 find $search_dir -maxdepth 5 -name *$run_id | while read wandb_dir_rel; do
     wandb_dir=$(realpath $wandb_dir_rel)
@@ -13,9 +13,10 @@ find $search_dir -maxdepth 5 -name *$run_id | while read wandb_dir_rel; do
 
     # find wandb dir pickle files
     wandb_pickles=$(find $wandb_dir/files -maxdepth 1 -name *.pickle -print | wc -l)
+    echo "$wandb_dir,$wandb_dir/files,$wandb_pickles"
 
     # find hydra dir pickle files
     hydra_pickles=$(find $hydra_dir -maxdepth 1 -name *.pickle -print | wc -l)
 
-    echo "$hydra_dir,$wandb_dir,$hydra_pickles,$wandb_pickles"
+    echo "$wandb_dir,$hydra_dir,$hydra_pickles"
 done
